b0d80d61e819d00f1805a42aef3b306ba55da3f4
mathlima <matheus.araujo@activesoft.com.br>
2024-03-14 15:50:48 -0300

    refactor: Adicionando typescript ao projeto

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from './HomeScreen';
 
 const Stack = createNativeStackNavigator();
