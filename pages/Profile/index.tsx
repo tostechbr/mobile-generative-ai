129b9e4b1724a004e46e91af9637c3814583d927
labinfo1 <labinfo1@ectufrn.ect.ufrn.br>
2024-04-09 13:21:52 -0600

    profile update, add modal, fix correct flatlist

--- pages/Profile/index.tsx
+++ pages/Profile/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Text,
     Image,
-    ScrollView,
     StyleSheet,
     TouchableOpacity,
-    useWindowDimensions,
     FlatList,
+    Modal,
+    Button,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
@@ -24,8 +24,13 @@ interface Card {
     text: string;
 }
 
-interface CardProps {
+interface CardComponentProps {
     card: Card;
+    onPress: () => void;
+}
+
+function truncateText(text: string, limit: number): string {
+    return text.length > limit ? text.substring(0, limit) + '...' : text;
 }
 
 const ProfileHeader: React.FC<{ name: string; imageUrl: string }> = ({ name, imageUrl }) => {
@@ -37,10 +42,33 @@ const ProfileHeader: React.FC<{ name: string; imageUrl: string }> = ({ name, ima
     );
 };
 
+const CardComponent: React.FC<CardComponentProps> = ({ card, onPress }) => {
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <View style={styles.cardContainer}>
+                <Image source={{ uri: card.imageUrl }} style={styles.cardImage} resizeMode="cover" />
+                <View style={styles.textContainer}>
+                    <Text>{truncateText(card.text, 100)}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+};
 
+export default function ProfileScreen() {
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [selectedCard, setSelectedCard] = useState<Card | null>(null);
 
+    const handleCardPress = (card: Card) => {
+        setSelectedCard(card);
+        setModalVisible(true);
+    };
 
-const ProfileScreen: React.FC = () => {
+    const closeModal = () => {
+        setModalVisible(false);
+        setSelectedCard(null);
+    };
+
     const navigation = useNavigation();
     const profileData: ProfileData = {
         name: 'Nome do usuario',
@@ -54,20 +82,29 @@ const ProfileScreen: React.FC = () => {
             {
                 id: 2,
                 imageUrl: 'https://via.placeholder.com/100',
+                text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry '
+            },
+            {
+                id: 3,
+                imageUrl: 'https://via.placeholder.com/100',
+                text: 'Lorem Ipsum'
+            },
+            {
+                id: 4,
+                imageUrl: 'https://via.placeholder.com/100',
+                text: 'Lorem Ipsum'
+            },
+            {
+                id: 5,
+                imageUrl: 'https://via.placeholder.com/100',
+                text: 'Lorem Ipsum'
+            },
+            {
+                id: 6,
+                imageUrl: 'https://via.placeholder.com/100',
                 text: 'Lorem Ipsum'
             },
         ]
-    };
-
-    const CardComponent = (card: Card) => {
-        return (
-            <View style={styles.cardContainer}>
-                <Image source={{ uri: card.imageUrl }} style={[styles.cardImage]} resizeMode="cover" />
-                <View style={styles.textContainer}>
-                    <Text>{card.text}</Text>
-                </View>
-            </View>
-        );
     };
 
     return (
@@ -76,27 +113,84 @@ const ProfileScreen: React.FC = () => {
                 <AntDesign name="arrowleft" size={24} color="black" />
             </TouchableOpacity>
             <ProfileHeader name={profileData.name} imageUrl={profileData.imageUrl} />
-            <FlatList
-                data={profileData.cards}
-                style={styles.containerChildren}
-                renderItem={CardComponent}
-            />
-
+            <View style={styles.containerDaddy}>
+                <FlatList
+                    data={profileData.cards}
+                    style={styles.containerChildren}
+                    renderItem={({ item }) => (
+                        <CardComponent card={item} onPress={() => handleCardPress(item)} />
+                    )}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+            </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={isModalVisible}
+                onRequestClose={closeModal}
+            >
+                <View style={styles.modalOverlay}>
+                    <View style={styles.modalView}>
+                        <Image source={{ uri: selectedCard?.imageUrl }} style={styles.modalImage} />
+                        <Text style={styles.modalText}>{selectedCard?.text}</Text>
+                        <Button title="Close" onPress={closeModal} />
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
-};
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#E55733',
     },
-    containerChildren: {
-        padding: 20,
+    containerDaddy: {
         flex: 1,
         backgroundColor: '#fff',
         borderTopLeftRadius: 40,
         borderTopRightRadius: 40
+    },
+    containerChildren: {
+        padding: 20,
+        marginBottom: 90,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40
+    },
+    modalOverlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fundo escurecido com opacidade
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        justifyContent: 'center',
+        position: 'absolute',
+        width: '80%',
+    },
+    modalImage: {
+        width: 100,
+        height: 100,
+        marginBottom: 20,
+    },
+    modalText: {
+        fontSize: 16,
+        marginBottom: 20,
     },
     backButton: {
         margin: 10,
@@ -140,4 +234,3 @@ const styles = StyleSheet.create({
     },
 });
 
-export default ProfileScreen;
